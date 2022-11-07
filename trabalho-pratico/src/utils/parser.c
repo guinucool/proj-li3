f431157f5a3236e76fea3056b7e1652b6edd4a92
guinucool <guinucool@gmail.com>
2022-11-07 00:20:05 +0000

    Criação da função de leitura de catálogos.

--- trabalho-pratico/src/utils/parser.c
+++ trabalho-pratico/src/utils/parser.c
@@ -1,24 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define _MAX_LINE 1000
+#include <string.h>
+#define _MAX_LINE_ 150
+
+void readCatalogue(char * path, char ctl)
+{
+    char * true_path = malloc(sizeof(char) * 22);
+
+    strcpy(true_path, path);
+
+    switch (ctl)
+    {
+        case 0:
+            strcat(true_path, "/users.csv");
+            break;
+
+        case 1:
+            strcat(true_path, "/drivers.csv");
+            break;
+
+        case 2:
+            strcat(true_path, "/rides.csv");
+            break;
+    }
+
+    FILE * catalogue = fopen(true_path, "r");
+    char * line = malloc(sizeof(char) * _MAX_LINE_);
+    int size = 0;
+
+    fgets(line, _MAX_LINE_, catalogue);
+
+    for (int i = 0; line[i] != '\n'; i++)
+        if(line[i] == ';' || line[i+1] == '\n') size++;
+
+    char * holder = malloc(sizeof(char));
+    char out[size][_MAX_LINE_];
+
+    while (fgets(line, _MAX_LINE_, catalogue) != NULL)
+    {
+        int f = 0, j = 0;
+
+        for (int i = 0; line[i] != '\n'; i++)
+        {
+            holder[i-j] = line[i];
+
+            if (line[i+1] == ';' || line[i+1] == '\n')
+            {
+                holder[i-j+1] = '\0';
+                strcpy(out[f], holder);
+                f++;
+                if (line[i+1] != '\n') i++;
+                j = i+1;
+            }
+        }
+    }
+}
 
 int main()
 {
-    FILE* data = fopen("../../db/users.csv", "r");
-    int loop = 1;
-    char *line = malloc(sizeof(char**) * _MAX_LINE);
+    readCatalogue("../../db", 0);
 
-    fgets(line, _MAX_LINE, data);
+    /*FILE* data = fopen("../../db/users.csv", "r");
+    int loop = 1;
+    char *line = malloc(sizeof(char**) * _MAX_LINE_);
+
+    fgets(line, _MAX_LINE_, data);
 
     while(loop)
     {   
         line[0] = '\0';
 
-        fgets(line, _MAX_LINE, data);
+        fgets(line, _MAX_LINE_, data);
         printf("%s", line);
 
         if (line[0] == '\0') loop = 0;
     }
 
-    return(0); 
+    return(0); */
 }
