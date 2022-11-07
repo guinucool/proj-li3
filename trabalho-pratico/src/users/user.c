86d2b62e8e9d12fbd655097d49ac5e0688eb5205
Gustavo Castro <a100482@alunos.uminho.pt>
2022-11-07 11:47:37 +0000

    atualizações nos ficheiros de user

--- trabalho-pratico/src/users/user.c
+++ trabalho-pratico/src/users/user.c
@@ -5,17 +5,16 @@
 
 
 
-User* userCreate(char* data_str){
+User* userCreate(char** data){
 
     User* user = (User*) malloc(sizeof(User));
-    char* tables[7] = string_division(data_str);
-    strncpy(user->username, tables[0], MAX_USER_STR);
-    strncpy(user->name, tables[1], MAX_USER_STR);
-    user->gender = tables[2][0];
-    user->birth_date = str_to_int(tables[3]);
-    user->account_creation = str_to_int(tables[4]);
-    user->pay_method = tables[5];
-    user->account_status = tables[6][0];
+    strncpy(user->username, data[0], MAX_USER_STR);
+    strncpy(user->name, data[1], MAX_USER_STR);
+    user->gender = data[2][0];
+    user->birth_date = str_to_int(data[3]);
+    user->account_creation = str_to_int(data[4]);
+    user->pay_method = data[5];
+    user->account_status = data[6][0];
 
     return user;
 }
@@ -29,18 +28,4 @@ short int* str_to_int(char* str){
     date[2] = 1000*(str[6]-48)+100*(str[7]-48)+10*(str[8]-48)+(str[9]-48);
 
     return date;
-}
-
-char* string_division(char* data_str){
-
-    char* tables[7];
-    short int i;
-
-    for ( i = 0; i < 7; i++)
-    {
-        tables[1] = strtok(data_str,';');
-    }
-
-    return tables;
-    
 }
