6b4ceff9f0798c2cd436b747f3ac765f4d9aafd3
xanaccs <xanaccs@live.com.pt>
2022-11-12 10:37:24 +0000

    Info driver update.

--- trabalho-pratico/inc/drivers/driver.h
+++ trabalho-pratico/inc/drivers/driver.h
@@ -1,15 +1,37 @@
 #ifndef _DRIVER_H_
 #define _DRIVER_H_
 
-#define MAX_NAME_STR 50
+#define NAME_STR_SIZE 50
+#define LICENSE_PLATE_STR_SIZE 10
+#define CITY_STR_SIZE 30
+
+#define BASIC 0
+#define GREEN 1
+#define PREMIUM 2
+
+extern char car_class_str[][8];
+
+#define MALE 'M'
+#define FEMALE 'F'
+
+#define INACTIVE 0
+#define ACTIVE 1
+
+extern char account_status_str[][9];
 
 typedef struct _DRIVER_ {
-	int id;
-	char name[MAX_NAME_STR];
-	//id;name;birth_day;gender;car_class;license_plate;city;account_creation;account_status
+	int id;                                      // INTEGER
+	char name[NAME_STR_SIZE];                     // STRING
+	short birth_day[3];                          // {day, month, year}
+	char gender;                                 //  {'M' - MALE, 'F' - FEMALE}
+	char car_class;                              // {0 - BASIC, 1 - GREEN, 2 - PREMIUM}
+	char license_plate[LICENSE_PLATE_STR_SIZE];  // STRING
+	char city[CITY_STR_SIZE];                    // STRING
+	short account_creation[3];                   // {day, month, year}
+	char account_status;                         // {0 - INACTIVE, 1 - ACTIVE}
 } Driver;
 
-Driver *createDriver(int id, char *name);
+Driver *createDriver(int, char *, short[], char, char, char[], char[], short[], char);
 void destroyDriver(Driver*);
 void printDriver(Driver*);
 
--- trabalho-pratico/src/drivers/driver.c
+++ trabalho-pratico/src/drivers/driver.c
@@ -4,15 +4,32 @@
 
 #include "../../inc/drivers/driver.h"
 
-Driver *createDriver(int id, char *name) {
+char car_class_str[][8] = {"BASIC", "GREEN", "PREMIUM"};
+char account_status_str[][9] = {"INACTIVE", "ACTIVE"};
+
+Driver *createDriver(int id, char *name, short birth_day[], char gender, char car_class, char license_plate[], char city[], short account_creation[], char account_status) {
 	
 	Driver *driver = (Driver*) malloc(sizeof(Driver));
 
+	// TODO: Needs validating input data.
+
 	driver->id = id;
-	strncpy(driver->name, name, MAX_NAME_STR);
+	strncpy(driver->name, name, NAME_STR_SIZE);
+
+	driver->birth_day[0] = birth_day[0];
+	driver->birth_day[1] = birth_day[1];
+	driver->birth_day[2] = birth_day[2];
+	
+	driver->gender = gender;
+	driver->car_class = car_class;
+	strncpy(driver->license_plate, license_plate, LICENSE_PLATE_STR_SIZE);
+	strncpy(driver->city, city, CITY_STR_SIZE);
+	driver->account_creation[0] = account_creation[0];
+	driver->account_creation[1] = account_creation[1];
+	driver->account_creation[2] = account_creation[2];
+	driver->account_status = account_status; 
 
 	return driver;
-
 }
 
 void destroyDriver(Driver *driver) {
@@ -20,5 +37,15 @@ void destroyDriver(Driver *driver) {
 }
 
 void printDriver(Driver* driver) {
-	printf("Driver {id: %d, name: %s}", driver->id, driver->name);
+	printf("Driver {id: %d, name: %s, birth_date: %02d/%02d/%04d, gender: %c, car_class: %s, license_plate: %s, city: %s, account_creation: %02d/%02d/%04d, account_status: %s}", 
+		driver->id, 
+		driver->name,
+		driver->birth_day[0], driver->birth_day[1], driver->birth_day[2],
+		driver->gender,
+		car_class_str[driver->car_class],
+		driver->license_plate,
+		driver->city,
+		driver->account_creation[0], driver->account_creation[1], driver->account_creation[2],
+		account_status_str[driver->account_status]
+	);
 }
