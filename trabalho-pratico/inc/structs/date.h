44043ec90e5b10b91b4ad2c2b12ab57fa1aa3830
Gustavo Castro <a100482@alunos.uminho.pt>
2022-11-14 18:00:57 +0000

    data struct criado

---
+++ trabalho-pratico/inc/structs/date.h
@@ -0,0 +1,10 @@
+#ifndef _DATE_H_
+#define _DATE_H_
+
+typedef struct _DATE_{
+    short data[3];
+    void* key;
+    char type;
+}Data;
+
+#endif
---
+++ trabalho-pratico/src/structs/date.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "../../inc/structs/date.h"
+
+Date * createDate(short* date, void* key, char type){
+    Date* d = (Date*) malloc(sizeof(Date));
+
+    d->date[0] = date[0];
+    d->date[1] = date[1];
+    d->date[2] = date[2];
+    d->key = key;
+    d->type = type;
+    
+    return d;
+}
+
