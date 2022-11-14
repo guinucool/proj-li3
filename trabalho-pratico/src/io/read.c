2ff75c49069521f856797aaa421c4580533560ad
guinucool <guinucool@gmail.com>
2022-11-14 17:50:34 +0000

    Assosiação das rides com a leitura de ficheiros, mais testagem do mesmo processo.

--- trabalho-pratico/src/io/read.c
+++ trabalho-pratico/src/io/read.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../../inc/io/read.h"
 #include "../../inc/structs/ride.h"
+
+void stringToDate(char* str,short int* date)
+{
+    date[0] = (short)(10*(str[0]-48)+(str[1]-48));
+    date[1] = (short)(10*(str[3]-48)+(str[4]-48));
+    date[2] = (short)(1000*(str[6]-48)+100*(str[7]-48)+10*(str[8]-48)+(str[9]-48));
+}
 
 void interUser(char args[][MAX_LINE], Global * glob)
 {
@@ -19,7 +27,7 @@ void interRide(char args[][MAX_LINE], Global * glob)
 
     Ride * ride = createRide(atoi(args[0]), date, atoi(args[2]), args[3], args[4], (short)atoi(args[5]), (short)atoi(args[6]), (short)atoi(args[7]), atof(args[8]), args[9]);
 
-    printf("%d\n");
+    printf("%s\n", ride->user);
 }
 
 void interCmd(char args[][MAX_LINE], Global * glob)
