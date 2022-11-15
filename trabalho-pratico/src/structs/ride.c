f3e6f0e273498193c88095671a38573d21a8e5e3
guinucool <guinucool@gmail.com>
2022-11-15 18:26:07 +0000

    Documentação da ride.c

--- trabalho-pratico/src/structs/ride.c
+++ trabalho-pratico/src/structs/ride.c
@@ -4,6 +4,36 @@
 #include "../../inc/utils/utilities.h"
 #include "../../inc/rides/ride.h"
 
+/// @brief A função createRide cria uma variável do tipo ride.
+/**
+ * A função createRide cria uma variável do tipo ride, alocando
+ * o espaço necessário na memória para a mesma.
+ * 
+ * Assim sendo, irá depois, também, associar os respetivos valores de input
+ * da função às repetivas propriedades da variável.
+ * 
+ * @param id O id da ride.
+ * 
+ * @param dte A data da ride.
+ * 
+ * @param drv O id do driver da ride.
+ * 
+ * @param usr O username do user da ride.
+ * 
+ * @param cty A cidade da ride.
+ * 
+ * @param dst A distância da ride.
+ * 
+ * @param scu O score dado ao user da ride.
+ * 
+ * @param scd O score dado ao driver da ride.
+ * 
+ * @param tip A gorjeta dada na ride.
+ * 
+ * @param cmt O comentário da ride.
+ * 
+ * @return A ride criada com as respetivas propriedades.
+ */
 Ride * createRide(int id, short dte[3], int drv, char * usr, char * cty, short dst, short scu, short scd, float tip, char * cmt)
 {
     Ride * ride = (Ride*) malloc(sizeof(Ride));
@@ -24,6 +54,13 @@ Ride * createRide(int id, short dte[3], int drv, char * usr, char * cty, short d
     return ride;
 }
 
+/// @brief A função destroyRide destroí uma variável do tipo ride.
+/**
+ * A função destroyRide destroí uma variável do tipo ride, libertando
+ * o espaço ocupado pela variável e pelas suas propriedades.
+ * 
+ * @param ride A variável do tipo ride que vai ser destruída.
+ */
 void destroyRide(Ride * ride)
 {
     free(ride);
