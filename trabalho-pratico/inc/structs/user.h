2af72a7558ced4f18e949b3359a77e4d2564db32
gustavo21castro <gustavo-castro@outlook.pt>
2022-11-18 16:45:36 +0000

    Documentação structs .h

--- trabalho-pratico/inc/structs/user.h
+++ trabalho-pratico/inc/structs/user.h
@@ -5,15 +5,16 @@
 
 #define MAX_USER_STR 50
 
+/// \struct Estrutura que define as variáveis do tipo User.
 typedef struct USER
 {
-    char username[MAX_USER_STR];
-    char name[MAX_USER_STR];
-    char gender;
-    short int birth_date[3]; // {day,month,year}
-    short int account_creation[3]; // {day,month,year}
-    char pay_method[MAX_USER_STR];
-    char account_status; // 'a' para active 'i' para inactive
+    char username[MAX_USER_STR];    //!< Username do user
+    char name[MAX_USER_STR];        //!< Nome do user
+    char gender;                    //!< Genero do user
+    short int birth_date[3];        //!< Data de nascimento do user no formato {dia,mês,ano}
+    short int account_creation[3];  //!< Data de criação de conta do user no formato {dia,mês,ano}
+    char pay_method[MAX_USER_STR];  //!< Metodo de pagamento que o user usa
+    char account_status;            //!< Status da conta ('a' para active 'i' para inactive)
 }User;
 
 User* userCreate(char data[][200]);
