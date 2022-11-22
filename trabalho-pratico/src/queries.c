e6b58cf4d6f1fde5bb40293e72488a8e6fb1e7d7
guinucool <guinucool@gmail.com>
2022-11-22 20:17:43 +0000

    Documentação da função query6.

--- trabalho-pratico/src/queries.c
+++ trabalho-pratico/src/queries.c
@@ -182,6 +182,29 @@ double query5(short* dateA , short* dateB, Global* glob)
     return result;
 }
 
+/// @brief A função query6 calcula a média da distância percorrida
+///        numa cidade dentro de um dado intervalo de tempo.
+/**
+ * A função query6 calcula a média da distância percorrida
+ * numa cidade dentro de um dado intervalo de tempo. 
+ * 
+ * Para isso, irá encontrar a lista (filtro) referente à cidade
+ * desejada, e irá percorrer as ocorrências desta uma a uma.
+ * 
+ * A cada ocorrência, irá verificar se a viagem referente à mesma
+ * tem uma data compatível com o intervalo desejado, e usar o valor
+ * da sua distância para o cálculo da média caso seja compatível.
+ * 
+ * @param cty A cidade a ser filtrada.
+ * 
+ * @param dateInf O limite inferior do intervalo.
+ * 
+ * @param dateUp O limite superior do intervalo.
+ * 
+ * @param glob A global a ser considerada para os cálculos estatísticos.
+ * 
+ * @return A média da distância total percorrida desejada.
+ */
 double query6(char * cty, short * dateInf, short * dateUp, Global * glob)
 {
     HashmapNode * cityList = (HashmapNode *) get(global_Hashmap(glob, 'c'), cty, equal_str, hashKey_Str, 0);
